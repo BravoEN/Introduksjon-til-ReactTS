b661d3b2059eef3052fd0b9b2387efb4ccfd9859
Eskil Nilsen <eskilnilsen2004@gmail.com>
2026-03-12 21:09:02 +0100

    det er gjort

--- src/pages/Oppg1.tsx
+++ src/pages/Oppg1.tsx
@@ -3,8 +3,38 @@ import {useState} from "react"
 import axios from "axios"
 import React from "react"
 
-import "./pages.css"
+import "./Pages.css"
 //import "../components/ComponentsExamples.css"
 
 export default function Oppg1(){
+
+    const {data, refetch, error} = useQuery({
+        queryKey:["cats"],
+        queryFn:() => getCatFact(), //bruker arrow function for å sende parameteret
+        staleTime: Infinity,
+    })
+
+    const [faktaCounter, setFaktaCounter] = useState<number>(1)
+
+    if (error) {
+        alert("Noe gikk galt, prøv igjen senere")
+    }
+
+    const nyFakta = () => {
+        refetch()
+        setFaktaCounter(faktaCounter + 1) // er en bug med funksjonen
+    }
+    
+    return(
+        <div>
+            <p>Fakta Counter: {faktaCounter}</p>
+            <p>{data?.data}</p>
+            <button onClick={nyFakta}>Ny kattefakta!</button>
+        </div>
+    )
+}
+
+const getCatFact = async () => {
+    const response = await axios.get("https://meowfacts.herokuapp.com/")
+    return await response.data
 }
--- src/pages/Oppg2.tsx
+++ src/pages/Oppg2.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import  { useQuery } from "@tanstack/react-query"
+import {useState} from "react"
+import axios from "axios"
+import React from "react"
 
 export default function Oppg2(){
+    const [nokAmount, setNokAmount] = useState<number>(0)
+
+    const {data} = useQuery({
+        queryKey:["dollar"],
+        queryFn:() => getDollar()
+    })
+
+    const usdAmount = nokAmount * (data?.nok.usd || 0)
+
     return(
-        <div><p>Oppg2</p></div>
+        <div>
+            <p>Konversjons raten er(nok til usd): {data?.nok.usd}</p>
+            <input
+                onChange={(e) => setNokAmount(Number(e.target.value))}
+                placeholder="Skriv inn NOK beløp"
+            />
+            <p>{nokAmount} NOK = {usdAmount.toFixed(2)} USD</p>
+        </div>
     )
+}
+
+const getDollar = async () => {
+    const response = await axios.get("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/nok.json")
+    return await response.data
 }
